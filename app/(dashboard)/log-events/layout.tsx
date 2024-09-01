1165f0caf7c3600688baeceb51d9263c3457fd3e
Razi Abdul Rasheed <razi@peers-solutions.com>
2024-09-01 22:08:26 +0200

    feat: dashboard layout style

--- app/(dashboard)/log-events/layout.tsx
+++ app/(dashboard)/log-events/layout.tsx
@@ -11,11 +11,11 @@ export default function Layout({
     <section className="h-full">
       {children}
       <article className="mx-14 mt-1 mb-3">
-        <h3 className="mb-1 text-sm">Log Events Histogram</h3>
+        <h3 className="mb-3 text-sm">Log Events Histogram</h3>
         {histogram}
       </article>
       <article className="mx-14 pb-4 pt-2 h-[calc(100%-16rem)]">
-        <h3 className="mb-1 text-sm">Log Event Table</h3>
+        <h3 className="mb-3 text-sm">Log Event Table</h3>
         <main className="h-[calc(100%-1.5rem)]">{table}</main>
       </article>
     </section>
